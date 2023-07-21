27f9146999bcc4b89ee1d988423eb65c368c2426
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-07-21 12:16:37 +0330

    merge sort is added

---
+++ Merge.js
@@ -0,0 +1,42 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
+}
+
+function merge(leftArr, rightArr) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+    if (leftArr[leftIndex] < rightArr[rightIndex]) {
+      result.push(leftArr[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(rightArr[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  while (leftIndex < leftArr.length) {
+    result.push(leftArr[leftIndex]);
+    leftIndex++;
+  }
+
+  while (rightIndex < rightArr.length) {
+    result.push(rightArr[rightIndex]);
+    rightIndex++;
+  }
+
+  return result;
+}
