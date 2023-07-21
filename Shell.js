d3173f4d11498fbfc0601839e6abbbbc979055c9
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-07-21 12:49:00 +0330

    shell sort is added

---
+++ Shell.js
@@ -0,0 +1,19 @@
+function shellSort(arr) {
+  const n = arr.length;
+
+  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    for (let i = gap; i < n; i++) {
+      const temp = arr[i];
+      let j = i;
+
+      while (j >= gap && arr[j - gap] > temp) {
+        arr[j] = arr[j - gap];
+        j -= gap;
+      }
+
+      arr[j] = temp;
+    }
+  }
+
+  return arr;
+}
